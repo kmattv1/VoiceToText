db5d8bce4ecbb5bd7add7bb523f906ee62e5e3f7
matt <k.mattv1@gmail.com>
2018-03-26 01:03:20 +0200

    and python file server

--- App.js
+++ App.js
@@ -4,74 +4,135 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
+    Platform,
+    StyleSheet,
+    Text,
+    View,
     Button,
-    SafeAreaView
+    SafeAreaView,
+    Alert
 } from 'react-native';
+
 import SoundRecorder from 'react-native-sound-recorder';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+    android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
+var UploadFile = require('NativeModules').UploadFile;
+
+function upload(file) {
+    var obj = {
+        uploadUrl: 'http://192.168.16.102:8000/', //https://test.com/upload/
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+        },
+        files: [
+            {
+                name: 'files', //key name
+                filename: file.name, //the name which server will receive
+                filepath: file.path, //local file path
+                filetype: file.type, //file type
+            },
+        ],
+        fields: { //additional parameters
+            filename: JSON.stringify(file.name)
+        },
+    };
+    Alert.alert('Upload File', '', [{
+        text: 'OK', onPress: () => {
+        }
+    }], {cancelable: true});
+
+    UploadFile.upload(obj, (returnCode, returnMessage, resultData) => {
+        Alert.alert(returnCode.toString(), returnMessage + ' ' + JSON.stringify(resultData), [{
+            text: 'OK',
+            onPress: () => {
+            }
+        }], {cancelable: true});
+        if (returnCode == 201) {
+        }
+        else {
+        }
+    })
+}
+
 type Props = {};
 export default class App extends Component<Props> {
-    startRecording(){
-        SoundRecorder.start(SoundRecorder.PATH_CACHE + '/test.mp4')
-            .then(function() {
+
+    startRecording() {
+        SoundRecorder.start(
+            SoundRecorder.PATH_CACHE + '/test.aac',
+            {
+                source: SoundRecorder.SOURCE_MIC,
+                format: SoundRecorder.FORMAT_AAC_ADTS,
+                encoder: SoundRecorder.ENCODER_AAC
+            })
+            .then(function () {
+                Alert.alert('Recording started', SoundRecorder.PATH_CACHE, [{
+                    text: 'OK', onPress: () => {
+                    }
+                }], {cancelable: true});
                 console.log('started recording');
             });
     }
 
-    stopRecording(){
+    stopRecording() {
         SoundRecorder.stop()
-            .then(function(path) {
+            .then(function (response) {
+                Alert.alert('Recording stoped', JSON.stringify(response), [{
+                    text: 'OK', onPress: () => {
+                    }
+                }], {cancelable: true});
+                upload({
+                    name: 'test',
+                    path: response.path,
+                    type: 'audio/aac'
+                });
                 console.log('stopped recording, audio file saved at: ' + JSON.stringify(path));
             });
     }
 
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-          <Button title="Start" color="green" onPress={this.startRecording}/>
-          <Button title="Stop" color="red" onPress={this.stopRecording}/>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </SafeAreaView>
-    );
-  }
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <Button title="Start" color="green" onPress={this.startRecording}/>
+                <Button title="Stop" color="red" onPress={this.stopRecording}/>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit App.js
+                </Text>
+                <Text style={styles.instructions}>
+                    {instructions}
+                </Text>
+            </SafeAreaView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
--- android/app/src/main/java/com/voicetext/MainApplication.java
+++ android/app/src/main/java/com/voicetext/MainApplication.java
@@ -3,45 +3,47 @@ package com.voicetext;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.kevinresol.react_native_sound_recorder.RNSoundRecorderPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactlibrary.RNSoundRecorderPackage;
+import com.yoloci.uploadfile.UploadFilePackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new RNSoundRecorderPackage(),
+                    new UploadFilePackage()
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNSoundRecorderPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
     }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
